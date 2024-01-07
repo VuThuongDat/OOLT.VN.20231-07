2397b9a7511c96d9c169766e437b36ed530e3091
Taduc2003 <buitaduc139@gmail.com>
2024-01-07 17:45:10 +0700

    quit button

--- sourcecode/TreeDataStructures/src/trees/controllers/View.java
+++ sourcecode/TreeDataStructures/src/trees/controllers/View.java
@@ -1,7 +1,10 @@
 package trees.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import trees.gui.GenericT.GtVisualiser;
 import trees.gui.avl.AvlVisualiser;
@@ -17,9 +20,29 @@ public class View {
     @FXML
     private Button bbt;
     @FXML
-    
-    private void gtAction() { gt.setOnAction(e->setStage1(new GtVisualiser()));}
+    private Button quit;
+    @FXML
+    void quitAction(ActionEvent event) {
+        // Hiển thị hộp thoại xác nhận
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Confirm Quit");
+        alert.setContentText("Are you sure you want to quit?");
 
+        ButtonType yesButton = new ButtonType("Yes");
+        ButtonType noButton = new ButtonType("No");
+
+        alert.getButtonTypes().setAll(yesButton, noButton);
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == yesButton) {
+                // Thực hiện hành động thoát ứng dụng ở đây
+                System.exit(0);
+            }
+        });
+    }
+    @FXML
+    private void gtAction() { gt.setOnAction(e->setStage1(new GtVisualiser()));}
     @FXML
     private void bstAction(){
         bst.setOnAction(e-> setStage(new BstVisualiser()));
